eeb3c8dc1a633ecbcde21e9c2551f004f0b1c323
Menkov Leonid <asdcleaner@yandex.ru>
2024-08-08 18:54:45 +0300

    fixed template, edited api

--- saby_combat/views.py
+++ saby_combat/views.py
@@ -5,8 +5,7 @@ from .models import Users, Upgrades, UserVerification, Levels, UserCoins, UserIn
 from .forms import LoginForm, RegisterForm
 from .utils import (add_new_user, get_user_by_username, get_user_by_email, is_user_confirmed, confirm_user_email,
                     send_confirmation_email, get_user_coins, submit_clicks_to_db, get_upgrades, delete_upgrades,
-                    patch_upgrades, create_upgrades, get_user_upgrades,
-                    delete_user_upgrades, patch_user_upgrades, purchase_user_upgrades)
+                    patch_upgrades, create_upgrades, get_user_upgrades, patch_user_upgrades, purchase_user_upgrades)
 from .verification_token import generate_verification_token, confirm_verification_token
 from .decorators import logout_required, confirm_your_email, admin_required
 
@@ -130,56 +129,27 @@ def profile_page():
 @app.route('/api/upgrades', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 def manage_upgrades():
     if request.method == 'POST':
-        if request.is_json:
-            upgrade = request.json
-            creation_data = create_upgrades(upgrade)
-            return jsonify(creation_data), 201
-        return jsonify({'message': 'Incorrect request, request body is not json'}), 400
+        response, status_code = create_upgrades(request)
+        return jsonify(response), status_code
     elif request.method == 'PATCH':
-        if request.is_json:
-            upgrade_modification = request.json
-            patching_data = patch_upgrades(upgrade_modification)
-            return jsonify(patching_data), 200
-        return jsonify({'message': 'Incorrect request, request body is not json'}), 400
+        response, status_code = patch_upgrades(request)
+        return jsonify(response), status_code
     elif request.method == 'DELETE':
-        if request.args and 'id' in request.args:
-            upgrade_ids = request.args.getlist('id')
-            deletion_data = delete_upgrades(upgrade_ids)
-            return jsonify(deletion_data), 200
-        return jsonify({'message': 'You are not specified entries to delete'}), 400
+        response, status_code = delete_upgrades(request)
+        return jsonify(response), status_code
     else:
-        if request.args and 'id' in request.args:
-            upgrade_ids = request.args.getlist('id')
-            specified_upgrades = get_upgrades(upgrade_ids)
-            return jsonify(specified_upgrades), 200
-        all_upgrades = get_upgrades()
-        return jsonify(all_upgrades), 200
+        response, status_code = get_upgrades(request)
+        return jsonify(response), status_code
 
 
-@app.route('/api/user_upgrades', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+@app.route('/api/user_upgrades', methods=['GET', 'POST', 'PATCH'])
 def manage_user_upgrades():
     if request.method == 'POST':
-        if request.args and 'user_id' in request.args and 'upgrade_id' in request.args:
-            purchase_data = purchase_user_upgrades(user_id=request.args['user_id'], upgrade_id=request.args['upgrade_id'])
-            return jsonify(purchase_data), 200
-        return {'message': 'You are not specified parameters'}, 400
+        response, status_code = purchase_user_upgrades(request)
+        return jsonify(response), status_code
     elif request.method == 'PATCH':
-        if request.is_json:
-            modification = request.json
-            patching_data = patch_user_upgrades(modification)
-            return jsonify(patching_data), 200
-        return jsonify({'message': 'Incorrect request, request body is not json'}), 400
-    elif request.method == 'DELETE':
-        if request.args and 'user_id' in request.args and 'upgrade_id' in request.args:
-            deletion_data = delete_user_upgrades(user_id=request.args['user_id'], upgrade_id=request.args['upgrade_id'])
-            return jsonify(deletion_data), 200
-        return jsonify({'message': 'You are not specified entry to delete'}), 400
+        response, status_code = patch_user_upgrades(request)
+        return jsonify(response), status_code
     else:
-        if request.args and 'user_id' in request.args and 'upgrade_id' in request.args:
-            single_entry = get_user_upgrades(user_id=request.args['user_id'], upgrade_id=request.args['upgrade_id'])
-            return jsonify(single_entry), 200
-        elif request.args and 'user_id' in request.args:
-            all_upgrades = get_user_upgrades(user_id=request.args['user_id'])
-            return jsonify(all_upgrades), 200
-        all_entries = get_user_upgrades()
-        return jsonify(all_entries), 200
+        response, status_code = get_user_upgrades(request)
+        return jsonify(response), status_code
